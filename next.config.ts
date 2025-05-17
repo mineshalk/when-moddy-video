dbe53aa7f0c4e8f1bc78e22c66e48d79608bec69
Eduard Plechkens <eduard@plechkens.com>
2025-05-17 08:02:44 +0300

    fix: baseUrl for normal domain

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/moddy-counter' : '',
 };
 
 export default nextConfig;
