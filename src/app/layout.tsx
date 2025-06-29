6bfeabfe6ecb1c98cd9e912aa33e84baeb88787d
Eduard Plechkens <work@plechkens.com>
2025-06-29 18:14:20 +0300

    build: add Google Analytics integration and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from "next";
+import { GoogleAnalytics } from '@next/third-parties/google'
 import ReactDOM from "react-dom";
 import "./globals.css";
-import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Модди, когда видео?",
@@ -49,21 +49,7 @@ export default function RootLayout({
             с любовью <span className="opacity-50">💙</span>
           </div>
         </footer>
-
-        <Script id="metrika-counter" strategy="afterInteractive">
-          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-    m[i].l=1*new Date();
-    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
- 
-    ym(103117456, "init", {
-          clickmap:true,
-          trackLinks:true,
-          accurateTrackBounce:true,
-          webvisor:true
-    });`}
-        </Script>
+        <GoogleAnalytics gaId="G-YREWNNXW2Q" />
       </body>
     </html>
   );
