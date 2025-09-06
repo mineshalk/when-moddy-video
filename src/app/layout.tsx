f9cefca5c38a82c679544cbb8dc2f20fb26d947d
Eduard Plechkens <work@plechkens.com>
2025-09-06 15:52:00 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
         </footer>
 
         <GoogleAnalytics gaId="G-YREWNNXW2Q" />
-        <Script type="text/javascript" strategy="afterInteractive">
+        <Script type="text/javascript" strategy="afterInteractive" id="metrica">
           {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
           m[i].l=1*new Date();
           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
